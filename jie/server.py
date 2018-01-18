6e8a261f8b5c4494ce49dd39784eb88899a29614
AuTa <auta520@live.com>
2018-01-18 18:27:53 +0800

    feat: modify server run method
    change getopt to argparse
    add function add_config for add config to app
    add function get_env for get environment from app
    add function run for run app

--- jie/server.py
+++ jie/server.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
-import getopt
+from argparse import ArgumentParser
 
 from asyncpg import create_pool
 
@@ -9,14 +9,38 @@ from sanic import Sanic
 from jie.environment import Environment
 
 app = Sanic(__name__)
+args = None
 
 
-@app.listener('before_server_start')
-async def register_db(app, loop):
+def get_args():
+    global args
+    if not args:
+        parser = ArgumentParser(prog='jie')
+        parser.add_argument('-c', dest='config', type=str, default='config.py')
+        args = parser.parse_args()
+    return args
+
+
+def add_config(app):
+    config_name = 'config.py'
+    args = get_args()
+    config_name = args.config
+    app.config.from_pyfile(config_name)
+
+
+def get_env(app, loop):
+    """Get environment from app. If environment is not existing, create it.
+    """
     env = getattr(app, 'env', None)
     if not env:
         env = Environment(app, loop)
         app.env = env
+    return env
+
+
+@app.listener('before_server_start')
+async def register_db(app, loop):
+    env = get_env(app, loop)
     if 'DB_CONFIG' not in app.config:
         return
     if 'max_size' not in app.config.DB_CONFIG:
@@ -25,17 +49,13 @@ async def register_db(app, loop):
     env.db_pool = db_pool
 
 
-if __name__ == '__main__':
-    config_name = 'config.py'
-    try:
-        options, args = getopt.getopt(sys.argv[1:], "hc:", ["help", "config="])
-    except getopt.GetoptError:
-        sys.exit()
-    for name, value in options:
-        if name in ('-c', '--config'):
-            config_name = value
-    app.config.from_pyfile(config_name)
+def run(app):
+    add_config(app)
     server_config = app.config.get('SERVER_CONFIG', {'host': '0.0.0.0', 'port': 8080})
     app.run(host=server_config.get('host', '0.0.0.0'),
             port=server_config.get('port', 8080), 
             debug=False)
+
+
+if __name__ == '__main__':
+    run(app)
