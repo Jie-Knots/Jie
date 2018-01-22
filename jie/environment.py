22d441346b1b24a935ead3607b42c9d742891a72
AuTa <auta520@live.com>
2018-01-22 10:04:37 +0800

    feat: add class Server for run server
    add class Server
    add class ServiceRegistry

--- jie/environment.py
+++ jie/environment.py
@@ -1,9 +1,10 @@
 class Environment:
 
-    def __init__(self, app, loop, db_pool=None):
+    def __init__(self, app, loop, host, port):
         self._app = app
         self._loop = loop
-        self._db_pool = db_pool
+        self._host = host
+        self._port = port
 
     @property
     def app(self):
@@ -14,9 +15,25 @@ class Environment:
         return self._loop
 
     @property
+    def host(self):
+        return self._host
+
+    @property
+    def port(self):
+        return self._port
+
+    @property
     def db_pool(self):
         return self._db_pool
 
     @db_pool.setter
     def db_pool(self, db_pool):
         self._db_pool = db_pool
+
+    @property
+    def registry(self):
+        return self._registry
+
+    @registry.setter
+    def registry(self, registry):
+        self._registry = registry
